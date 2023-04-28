ee315b3d445b70155776241fab9077f67cbb9931
Vitor Da Silva <vitor.pintodasilva@heig-vd.ch>
2023-04-28 11:24:15 +0200

    ajout page remerciement

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 const activeImage = document.querySelector(".product-image .active");
 const productImages = document.querySelectorAll(".image-list img");
 const navItem = document.querySelector('a.toggle-nav');
+const button = document.querySelector('.add-to-cart');
 
 function changeImage(e) {
   activeImage.src = e.target.src;
@@ -10,5 +11,11 @@ function toggleNavigation(){
   this.nextElementSibling.classList.toggle('active');
 }
 
+function changePage() {
+  window.location.href = "panier.html";
+}
+
 productImages.forEach(image => image.addEventListener("click", changeImage));
 navItem.addEventListener('click', toggleNavigation);
+
+button.addEventListener('click', changePage);
